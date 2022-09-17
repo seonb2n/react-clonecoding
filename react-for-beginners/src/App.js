328bad47aae3d740674b056893bbb5b2ef979f10
seonb2n <626ksb@gmail.com>
2022-09-17 16:06:06 +0900

    input 으로 입력한 것을 todo list 로 보여주도록 구현

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -2,29 +2,33 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import {useEffect, useState} from "react";
 
-function Hello() {
-    function effectCreFn() {
-        console.log("created :)");
-        return effectDesFn();
-    }
-
-    function effectDesFn() {
-        console.log("destroyed :)");
-    }
-
-    useEffect(effectCreFn, []);
-    return <h1>Hello</h1>
-}
-
 function App() {
-    const [showing, setShowing] = useState(false);
-    const onClick = () => setShowing((prev) => !prev);
-    return (
-        <div>
-            {showing ? <Hello /> : null}
-            <button onClick={onClick}>{showing ? "show" : "hide"}</button>
-        </div>
-    );
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+            return;
+        }
+        setToDos(currentArr => [toDo, ...currentArr]);
+        setToDo("");
+    };
+    console.log(toDos);
+    return <div>
+        <h1>My To Dos {toDos.length}</h1>
+        <form onSubmit={onSubmit}>
+            <input
+                onChange={onChange}
+                value={toDo}
+                type="text"
+                placeholder="Write to do.."
+            />
+            <button>Add to Do</button>
+        </form>
+        <hr />
+        {toDos.map((item, index) => <li key={index}>{item}</li>)}
+    </div>;
 }
 
 export default App;

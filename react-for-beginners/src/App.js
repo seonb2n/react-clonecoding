27a4512640e60cc40e76fcb36ed5fe29fa559268
seonb2n <626ksb@gmail.com>
2022-09-17 10:00:47 +0900

    useEffect 를 사용해서 render 제한하는 법 학습

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,29 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import {useEffect, useState} from "react";
 
 function App() {
+    const [counter, setCounter] = useState(0);
+    const [keyword, setKeyword] = useState("")
+    const onClick = () => setCounter((prev) => prev + 1);
+    const onChange = (event) => setKeyword(event.target.value);
+    useEffect(() => {
+        console.log("call the api");
+    }, []);
+    useEffect(() => {
+        console.log("I run when keyword changes");
+    }, [keyword]);
+    useEffect(() => {
+        console.log("I run when counter changes");
+    }, [counter]);
     return (
         <div>
-            <h1 className={styles.title}>Welcome to React!</h1>
-            <Button text={"Continue"}/>
+            <input value={keyword}
+                   onChange={onChange}
+                   type="text"
+                   placeholder="search here.."/>
+            <h1>{counter}</h1>
+            <button onClick={onClick}>click me</button>
         </div>
     );
 }

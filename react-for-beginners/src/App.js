84633df119a070ffe0ae8312e9deac97e26cc096
seonb2n <626ksb@gmail.com>
2022-09-17 10:31:08 +0900

    useEffect 를 사용해서 component 파괴될 때 return 확인

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -2,28 +2,27 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import {useEffect, useState} from "react";
 
+function Hello() {
+    function effectCreFn() {
+        console.log("created :)");
+        return effectDesFn();
+    }
+
+    function effectDesFn() {
+        console.log("destroyed :)");
+    }
+
+    useEffect(effectCreFn, []);
+    return <h1>Hello</h1>
+}
+
 function App() {
-    const [counter, setCounter] = useState(0);
-    const [keyword, setKeyword] = useState("")
-    const onClick = () => setCounter((prev) => prev + 1);
-    const onChange = (event) => setKeyword(event.target.value);
-    useEffect(() => {
-        console.log("call the api");
-    }, []);
-    useEffect(() => {
-        console.log("I run when keyword changes");
-    }, [keyword]);
-    useEffect(() => {
-        console.log("I run when counter changes");
-    }, [counter]);
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
     return (
         <div>
-            <input value={keyword}
-                   onChange={onChange}
-                   type="text"
-                   placeholder="search here.."/>
-            <h1>{counter}</h1>
-            <button onClick={onClick}>click me</button>
+            {showing ? <Hello /> : null}
+            <button onClick={onClick}>{showing ? "show" : "hide"}</button>
         </div>
     );
 }
